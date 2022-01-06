5ca3862bb6256527fe82b75cbee5c953c45a536e
orosztomi <93983032+orosztomi@users.noreply.github.com>
2022-01-06 10:40:12 +0100

    spring

---
+++ src/main/java/torpedo/configurations/ApplicationConfiguration.java
@@ -0,0 +1,14 @@
+package hu.nye.progtech.torpedo.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+@Configuration
+public class ApplicationConfiguration {
+
+
+
+}
